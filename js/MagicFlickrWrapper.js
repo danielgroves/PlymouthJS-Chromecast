187e4e1cde902c92dd209238e0e6646bf75349fc
Daniel Groves <hello@danielgroves.net>
2016-05-03 21:43:23 +0100

    Build the initial sender view.
    Forms the basis of the sender application by showing the available
    albums. This makes use of the Flickr JS wrapper, and adds a second
    wrapper on top of this to handle the awkward design.
    
    I'll tidy this up before the talk if I get time, but the current
    focus is just making something work as quickly as possible.

---
+++ js/MagicFlickrWrapper.js
@@ -0,0 +1,63 @@
+function MagicFlickrWrapper(userId) {
+  this.userId = userId;
+  this.images = new Array();
+}
+
+MagicFlickrWrapper.prototype.getPhotosets = function() {
+  var that = this;
+  flickr.photosets.getList({
+    user_id: this.userId
+  }, function(error, result) {
+    for(var i in result.photosets.photoset) {
+      var photoset = result.photosets.photoset[i];
+      that.images.push(new PhotosetHandler(that.userId, photoset));
+    }
+  });
+}
+
+function PhotosetHandler(userId, photoset) {
+  this.userId = userId;
+  this.photoset = photoset;
+  this.background = null;
+
+  this.getPhotos(photoset.id)
+}
+
+PhotosetHandler.prototype.getPhotos = function(photosetId) {
+  var that = this;
+
+  flickr.photosets.getPhotos({
+    photoset_id: photosetId,
+    user_id: that.userId
+  }, function(error, result) {
+    that.getPhotoSizes(result.photoset.photo[0].id);
+  });
+}
+
+PhotosetHandler.prototype.getPhotoSizes = function(photoId) {
+  var that = this;
+
+  flickr.photos.getSizes({
+    photo_id: photoId
+  }, function(error, result) {
+    that.background = result.sizes.size[9].source;
+    that.injectHtml();
+  });
+}
+
+PhotosetHandler.prototype.injectHtml = function() {
+  var article = document.createElement('article');
+  article.setAttribute('data-flickr-id', this.photoset.id);
+  article.style.backgroundImage = "url(" + this.background + ")";
+
+  var heading = document.createElement('h1');
+  heading.innerHTML = this.photoset.title._content;
+
+  var description = document.createElement('p');
+  description.innerHTML = this.photoset.description._content;
+
+  article.appendChild(heading);
+  article.appendChild(description);
+
+  document.getElementById('flickr-container').appendChild(article);
+}
